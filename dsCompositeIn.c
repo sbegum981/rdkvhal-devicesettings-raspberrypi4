0614011f615dc48f8537b8134e83e8370fffeb27
sbegum981 <Sameena_Begum@comcast.com>
2024-10-24 11:50:21 +0530

    Update dsCompositeIn.c

--- dsCompositeIn.c
+++ dsCompositeIn.c
@@ -11,46 +11,46 @@
 
 dsError_t dsCompositeInInit (void)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
 dsError_t dsCompositeInTerm (void)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
 dsError_t dsCompositeInGetNumberOfInputs (uint8_t *pNumberOfInputs)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
 dsError_t dsCompositeInGetStatus (dsCompositeInStatus_t *pStatus)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
 dsError_t dsCompositeInSelectPort (dsCompositeInPort_t Port)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
 dsError_t dsCompositeInScaleVideo (int32_t x, int32_t y, int32_t width, int32_t height)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
 dsError_t dsCompositeInRegisterConnectCB (dsCompositeInConnectCB_t CBFunc)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
 dsError_t dsCompositeInRegisterSignalChangeCB (dsCompositeInSignalChangeCB_t CBFunc)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
 dsError_t dsCompositeInRegisterStatusChangeCB (dsCompositeInStatusChangeCB_t CBFunc)
 {
-    dsError_t ret = dsERR_NONE;
+    dsError_t ret = dsERR_OPERATION_NOT_SUPPORTED;
     return ret;
 }
